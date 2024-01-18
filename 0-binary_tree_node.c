8e1a85025039dd3b95b6ab8cca8e164b462448b8
sachihiroo <saif3antar@gmail.com>
2024-01-18 15:16:50 +0100

    New node FIXED

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,6 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->right = NULL;
 	new_node->left = NULL;
-	new_node->value = value;
+	new_node->n = value;
 	return (new_node);
 }
