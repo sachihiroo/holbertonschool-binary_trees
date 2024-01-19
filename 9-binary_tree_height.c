309de7d733c277357cc5c99bdcab080ac47133ee
sachihiroo <saif3antar@gmail.com>
2024-01-19 11:27:51 +0100

    Height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height -
- *
- *
+ * binary_tree_height - a function that measures the height of a binary tree
+ * @tree: pointer
+ * Return: 0 or depth
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
