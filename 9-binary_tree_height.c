0d9175d15078099c4687e5e9361a2e09d67d2cc2
sachihiroo <saif3antar@gmail.com>
2024-01-22 15:40:08 +0100

    Height of a Tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int l = 0, r = 0;
 
-	if (tree == NULL || tree->parent == NULL)
+	if (tree == NULL)
 		return (0);
 
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
 
-		if (tree->right == NULL && tree->left == NULL)
-	{
+	if (tree->right == NULL && tree->left == NULL)
 		return (0);
-	}
 
 	if (l >= r)
 		return (l + 1);
