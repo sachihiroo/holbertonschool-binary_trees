0b1694883a474fff35a8621fc34951bf42200ab7
sachihiroo <saif3antar@gmail.com>
2024-03-31 19:44:05 +0100

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -46,4 +46,12 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+size_t binary_tree_height_2(const binary_tree_t *tree);
+int countNodes(const binary_tree_t *tree);
+int heightT(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+
+void binary_tree_print(const binary_tree_t *);
+
 #endif
